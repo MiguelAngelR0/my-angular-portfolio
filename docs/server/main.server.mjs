66757951495bfb118bdd26be2dd2863e329bdc20
MiguelAngelR0 <miguelangelrodriguezescorcia@gmail.com>
2025-05-02 13:43:09 +0200

    update docs

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-MP2VBYKT.mjs";import"./chunk-KDQOCY3M.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-3NHOPKOV.mjs";import"./chunk-KDQOCY3M.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
